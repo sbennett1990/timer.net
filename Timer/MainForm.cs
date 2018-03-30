230125c5e832b85f54cd7dc8a3091fd7114d383a
Scott Bennett <sbennett1990@gmail.com>
2018-03-30 14:18:38 -0400

    Now that the main functionality is closing in, remove unnecessary using's

--- Timer/MainForm.cs
+++ Timer/MainForm.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Media;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Timer.Utils;
 
